f5c52951992e4ddcb233a1e335c878a939e968f3
Indra H Setiawan <33475182+asingo@users.noreply.github.com>
2025-09-11 17:00:22 +0700

    add props component

--- resources/views/components/button/link.blade.php
+++ resources/views/components/button/link.blade.php
@@ -1,3 +1,8 @@
+@props([
+    'href' => '#',
+    'class' => '',
+    'outlined' => false,
+])
 <div class="{{$class}}">
     <a href="{{$href}}" class="py-3 px-6 {{$class}} {{$outlined ? 'bg-none text-white border border-white':'bg-primary text-white'}} font-semibold rounded-xl">{{$slot}}</a>
 </div>
