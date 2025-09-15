1505420a5c161d1e336a5c4eb438f716cc2986a0
Indra H Setiawan <33475182+asingo@users.noreply.github.com>
2025-09-15 09:17:11 +0700

    fix menu mobile

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -127,19 +127,50 @@
                     x-transition:leave-end="opacity-0 -translate-y-5"
                     class="absolute bg-white w-screen left-0 h-screen top-14 z-50"
                 >
-                    <ul class="py-2">
+                    <ul class="flex flex-col gap-5 mx-6 mt-4 pt-4 border-t">
                         <li>
-                            <a href="#"
-                               @click.prevent="selected = 'EN'; open = false"
-                               class="block px-4 py-2 hover:bg-gray-100">
-                                EN
+                            <a href="{{route('home')}}" class="relative group">
+                                <span :class="atTop && 'hover:!text-primary' ">About Us</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
                             </a>
                         </li>
                         <li>
-                            <a href="#"
-                               @click.prevent="selected = 'ID'; open = false"
-                               class="block px-4 py-2 hover:bg-gray-100">
-                                ID
+                            <a href="{{route('home')}}" class="relative group">
+                                <span :class="atTop && 'hover:!text-primary' ">Location</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('home')}}" class="relative group">
+                                <span :class="atTop && 'hover:!text-primary' ">Medical Professionals</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('home')}}" class="relative group">
+                                <span :class="atTop && 'hover:!text-primary' ">Health Screening</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('home')}}" class="relative group">
+                                <span :class="atTop && 'hover:!text-primary' ">Contact Us</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{route('home')}}" class="relative group">
+                        <span class="flex gap-2 items-center" :class="atTop && 'hover:!text-primary' ">
+                            <svg width="20" height="20" viewBox="0 0 20 20" class="fill-white"
+                                 :class="atTop && '!fill-red-500' "
+                                 xmlns="http://www.w3.org/2000/svg">
+<path
+    d="M11.3642 6.84333L11.2106 5H8.51919L8.36558 6.84333H5.3172L2 10.824V14.8311H3.75412C3.95525 15.5395 4.60749 16.06 5.37944 16.06C6.1514 16.06 6.8036 15.5395 7.00477 14.8311H12.725C12.9261 15.5395 13.5784 16.06 14.3503 16.06C15.1223 16.06 15.7745 15.5395 15.9757 14.8311H17.7298V6.84333H11.3642ZM5.37944 15.1383C4.95594 15.1383 4.61139 14.7938 4.61139 14.3703C4.61139 13.9468 4.95594 13.6022 5.37944 13.6022C5.80295 13.6022 6.1475 13.9468 6.1475 14.3703C6.1475 14.7938 5.80295 15.1383 5.37944 15.1383ZM7.22278 10.53H3.44474L5.74891 7.765H7.22278V10.53ZM9.29045 6.84333L9.36725 5.92167H10.3625L10.4393 6.84333H9.29045ZM13.4287 11.7589H12.507V10.8372H11.5853V9.91556H12.507V8.99389H13.4287V9.91556H14.3503V10.8372H13.4287V11.7589ZM14.3503 15.1383C13.9268 15.1383 13.5823 14.7938 13.5823 14.3703C13.5823 13.9468 13.9268 13.6022 14.3503 13.6022C14.7738 13.6022 15.1184 13.9468 15.1184 14.3703C15.1184 14.7938 14.7738 15.1383 14.3503 15.1383Z"
+/>
+</svg>
+
+                            Emergency 564 123</span>
+                                <span class="menu-interaction" :class="atTop && '!bg-primary' "></span>
                             </a>
                         </li>
                     </ul>
