d78c8a3008d44286c97a8c9b5a2c8bca10e0cfe5
Indra H Setiawan <33475182+asingo@users.noreply.github.com>
2025-11-23 21:52:44 +0700

    jempol jempol

--- resources/views/pages/location/index.blade.php
+++ resources/views/pages/location/index.blade.php
@@ -1,8 +1,8 @@
-@php use App\Models\Media; @endphp
+@php use App\Models\Media;use App\Models\Speciality; @endphp
 @extends('template')
 @section('content')
     <div class="max-w-screen-2xl mx-auto py-24 px-6 2xl:px-0 mt-6">
-        <x-breadcrumb parent="Home" child="{{$title}}" />
+        <x-breadcrumb parent="Home" child="{{$title}}"/>
         <div class="mt-8">
             <x-typography.subheading location="page">{{$page->content['section']['title']}}</x-typography.subheading>
             <x-typography.heading tag="h1" location="page">{{$page->content['section']['heading']}}
@@ -10,25 +10,33 @@
         </div>
         <div class="flex flex-col gap-10 mt-8 md:mt-14">
             @foreach($data as $v)
+                @php
+                $locale = app()->getLocale();
+                    $meta = Speciality::whereIn('id', $v->about_speciality)->get()->map(fn ($item) => $item->getTranslation('title', $locale))->toArray();
+                @endphp
+
                 <div class="grid grid-cols-1 md:grid-cols-2 md:gap-10 gap-3">
                     <div>
 
                         <x-image :id="$v->id" :class="'rounded-xl w-full object-cover aspect-[5/3]'"/>
-{{--                        <img class="rounded-xl w-full object-cover" src="{{asset($v->thumbnail)}}" alt=""/>--}}
+                        {{--                        <img class="rounded-xl w-full object-cover" src="{{asset($v->thumbnail)}}" alt=""/>--}}
                     </div>
                     <div class="flex flex-col gap-3 md:gap-5">
                         <h3 class="text-2xl md:text-3xl text-textsub font-semibold">{{$v->title}}</h3>
-                        <div class="text-textsub text-lg md:text-xl">{{$v->meta}}</div>
-                        <a href="{{$v->link_maps}}" class="text-primary hover:text-accent gap-2 items-center text-lg flex">
+                        <div class="text-textsub text-lg md:text-xl">{{implode(', ',$meta)}}</div>
+                        <a href="{{$v->link_maps}}"
+                           class="text-primary hover:text-accent gap-2 items-center text-lg flex">
                             <x-heroicon-s-map-pin class="w-7 h-7"/>
                             <span>{{__('building.maps')}}</span>
                             <x-heroicon-o-chevron-right class="w-7 h-7"/>
-                        </a> <a href="tel:{{filter_var($v->general_number, FILTER_SANITIZE_NUMBER_INT)}}" class="text-primary hover:text-accent gap-2 items-center text-lg flex">
+                        </a> <a href="tel:{{filter_var($v->general_number, FILTER_SANITIZE_NUMBER_INT)}}"
+                                class="text-primary hover:text-accent gap-2 items-center text-lg flex">
                             <x-heroicon-s-phone class="w-7 h-7"/>
                             <span>{{__('contact.us')}}</span>
                             <x-heroicon-o-chevron-right class="w-7 h-7"/>
                         </a>
-                        <x-button.link class="mt-2" href="{{request()->url().'/'.$v->slug}}">{{__('location')}}</x-button.link>
+                        <x-button.link class="mt-2"
+                                       href="{{request()->url().'/'.$v->slug}}">{{__('location')}}</x-button.link>
                     </div>
                 </div>
             @endforeach
