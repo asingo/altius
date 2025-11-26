6ff6e15a57a460d301a97480aae6f2d03ffc4be6
Indra H Setiawan <33475182+asingo@users.noreply.github.com>
2025-11-26 16:55:41 +0700

    responsive cta

--- resources/views/pages/medical-professional/single.blade.php
+++ resources/views/pages/medical-professional/single.blade.php
@@ -159,14 +159,14 @@
             </div>
 
         </div>
-        <div class=" fixed bottom-0 left-0 right-0">
-            <div class="bg-white px-6 py-2 drop-shadow-[0_-3px_10px_rgba(0,0,0,0.1)] rounded-t-2xl max-w-screen-2xl mx-auto flex justify-between items-center">
+        <div class="z-[99] fixed bottom-0 left-0 right-0">
+            <div class="bg-white px-6 py-2 drop-shadow-[0_-3px_10px_rgba(0,0,0,0.1)] rounded-t-2xl flex-col sm:flex-row max-w-screen-2xl mx-auto flex justify-between gap-2 sm:items-center">
                 <div>
-                    <h5 class="text-textsub">Book your Appointment now with</h5>
-                    <h4 class="font-bold">{{$title}}</h4>
+                    <p class="text-textsub">Book your Appointment now with</p>
+                    <h6 class="font-bold">{{$title}}</h6>
                 </div>
                 <div>
-                    <x-button.link href="#">Book Now</x-button.link>
+                    <x-button.link href="#" class="w-full sm:w-fit text-center">Book Now</x-button.link>
                 </div>
 
             </div>
