6bdcff8ca391521d55c041109cef6a040ff9e4ed
Indra H Setiawan <indraharisetiawan@gmail.com>
2025-12-15 13:48:37 +0700

    fix image id

--- resources/views/pages/offers/single.blade.php
+++ resources/views/pages/offers/single.blade.php
@@ -52,7 +52,7 @@
             <div class="grid md:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-12 items-stretch my-8">
                 @foreach($others as $d)
                     <x-grid.basic
-                        image="{{\Awcodes\Curator\Models\Media::find($d['image'])?->url}}"
+                        image="{{\Awcodes\Curator\Models\Media::find(get_image_id($d['image']))?->url}}"
                         heading="{!! $d['title'] !!}"
                         slug="{{localized_route('offers')}}/{{$d['slug']}}"
                     />
